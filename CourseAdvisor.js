c1f4cfa66ede05143598c208b3d3438a793688fc
jwchoi-ibm <jwchoi@ibm.com>
2019-09-27 12:52:41 -0400

    Add coursera faq functionality

--- CourseAdvisor.js
+++ CourseAdvisor.js
@@ -25,11 +25,54 @@ async function queryDiscovery(
 
     return queryResult;
   } catch (err) {
-    return { err: err.message };
+    throw new Error(err.message);
   }
 }
 
-const CourseAdvisor = {
+async function recommendCourse(queryResult) {
+  try {
+    const courses = [];
+
+    queryResult.results.forEach(result => {
+      while (courses.length <= 3) {
+        const course = {
+          name: result.name,
+          link: `https://www.coursera.org/learn/${result.slug}`,
+          description: result.description.split(".")[0]
+        };
+        courses.push(course);
+      }
+    });
+
+    if (courses.length === 0) {
+      throw new Error("No courses are found");
+    }
+
+    return { courses };
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+async function answerFaq(queryResult) {
+  try {
+    textContent = queryResult.results[0].text;
+    splitTextContent = textContent.split("\n\n");
+
+    if (splitTextContent.length >= 9) {
+      return {
+        res: splitTextContent[6] + splitTextContent[7] + splitTextContent[8]
+      };
+    }
+    throw new Error(
+      "Sorry, we are unable to find the answer from Help Centre."
+    );
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+const CourseraAdvisor = {
   async connectDiscovery(params) {
     const {
       discoveryUsername,
@@ -37,11 +80,21 @@ const CourseAdvisor = {
       environmentId,
       collectionId,
       input,
+      intent,
       url
     } = params;
 
     try {
-      const queryString = `enriched_description.keywords.text: ${input}`;
+      let enrichedField;
+      if (intent === "course_recommendation") {
+        enrichedField = "description";
+      } else if (intent === "faq") {
+        enrichedField = "text";
+      } else {
+        throw new Error(`Intent: ${intent} cannot be recognized`);
+      }
+
+      const queryString = `enriched_${enrichedField}.keywords.text: ${input}`;
 
       const queryResult = await queryDiscovery(
         environmentId,
@@ -52,32 +105,24 @@ const CourseAdvisor = {
         url
       );
 
-      const courses = [];
-
-      if (!queryResult.results) {
+      if (!queryResult) {
+        return "No query result found";
+      } else if (!queryResult.results) {
         return queryResult;
       }
 
-      queryResult.results.forEach(result => {
-        if (courses.length <= 3) {
-          const course = {
-            name: result.name,
-            link: `https://www.coursera.org/learn/${result.slug}`,
-            description: result.description.split(".")[0]
-          };
-          courses.push(course);
-        }
-      });
-
-      if (courses.length === 0) {
-        return { err: "No courses are found" };
+      let res;
+      if (intent === "course_recommendation") {
+        res = await recommendCourse(queryResult);
+      } else {
+        res = await answerFaq(queryResult);
       }
 
-      return { courses };
+      return res;
     } catch (err) {
       return { err: err.message };
     }
   }
 };
 
-module.exports = CourseAdvisor;
+module.exports = CourseraAdvisor;
